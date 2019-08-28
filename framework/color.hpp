dc4f0c752a27844704e5132270e4e897c6b689aa
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-08-28 23:53:11 +0200

    updated color formatting

--- framework/color.hpp
+++ framework/color.hpp
@@ -1,138 +1,118 @@
-// -----------------------------------------------------------------------------
-// Copyright  : (C) 2014-2017 Andreas-C. Bernstein
-// License    : MIT (see the file LICENSE)
-// Maintainer : Andreas-C. Bernstein <andreas.bernstein@uni-weimar.de>
-// Stability  : experimental
-//
-// Color
-// -----------------------------------------------------------------------------
-
-#ifndef BUW_COLOR_HPP
-#define BUW_COLOR_HPP
+#ifndef COLOR_HPP
+#define COLOR_HPP
 
 #include <iostream>
 
-struct Color
-{
-  Color() : r(0.0f), g(0.0f), b(0.0f) {}
-  Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
+struct Color {
 
-  friend std::ostream& operator<<(std::ostream& os, Color const& c)
-  {
+  float r;
+  float g;
+  float b;
+
+  Color()
+    : r(0.0f), g(0.0f), b(0.0f) {}
+  
+  Color(float red, float green, float blue)
+    : r(red), g(green), b(blue) {}
+
+  friend std::ostream& operator<<(std::ostream& os, Color const& c) {
     os << "(" << c.r << "," << c.g << "," << c.b << ")\n";
     return os;
   }
 
-  Color& operator+(Color const& other)
-  {
+  Color& operator+(Color const& other) {
     r += other.r;
     g += other.g;
     b += other.b;
     return *this;
   }
 
-  Color& operator-(Color const& other)
-  {
+  Color& operator-(Color const& other) {
     r -= other.r;
     g -= other.g;
     b -= other.b;
     return *this;
   }
 
-  Color& operator*(float value)
-  {
+  Color& operator*(float value) {
     r *= value;
     g *= value;
     b *= value;
     return *this;
   }
 
-  Color& operator/(float value)
-  {
+  Color& operator/(float value) {
     r /= value;
     g /= value;
     b /= value;
     return *this;
   }
 
-  Color& operator+=(Color const& other)
-  {
+  Color& operator+=(Color const& other) {
     r += other.r;
     g += other.g;
     b += other.b;
     return *this;
   }
 
-  Color& operator-=(Color const& other)
-  {
+  Color& operator-=(Color const& other) {
     r -= other.r;
     g -= other.g;
     b -= other.b;
     return *this;
   }
 
-  Color& operator*=(Color const& other)
-  {
+  Color& operator*=(Color const& other) {
     r *= other.r;
     g *= other.g;
     b *= other.b;
     return *this;
   }
 
-  Color& operator*=(float value)
-  {
+  Color& operator*=(float value) {
     r *= value;
     g *= value;
     b *= value;
     return *this;
   }
 
-  Color& operator/=(float value)
-  {
+  Color& operator/=(float value) {
     r /= value;
     g /= value;
     b /= value;
     return *this;
   }
 
-  friend Color operator+(Color const& a, Color const& b)
-  {
+  friend Color operator+(Color const& a, Color const& b) {
     auto tmp(a);
     tmp += b;
     return tmp;
   }
 
-  friend Color operator-(Color const& a, Color const& b)
-  {
+  friend Color operator-(Color const& a, Color const& b) {
     auto tmp(a);
     tmp -= b;
     return tmp;
   }
 
-  friend Color operator*(float value, Color const& color)
-  {
+  friend Color operator*(float value, Color const& color) {
     auto tmp(color);
     tmp *= value;
     return tmp;
   }
 
-  friend Color operator*(Color const& a, Color const& b)
-  {
+  friend Color operator*(Color const& a, Color const& b) {
     auto tmp(a);
     tmp *= b;
     return tmp;
   }
 
-  friend Color operator/(float value, Color const& color)
-  {
+  friend Color operator/(float value, Color const& color) {
     auto tmp(color);
     tmp /= value;
     return tmp;
   }
 
-  float r;
-  float g;
-  float b;
 };
 
-#endif //#define BUW_COLOR_HPP
+#endif //#define COLOR_HPP
